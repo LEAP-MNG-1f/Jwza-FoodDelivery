530c4631427f2c9a68caae2f62ff6f28cb136eeb
Jwz-jwz <jnglobal444@gmail.com>
2024-11-17 21:15:44 +0800

    header-fixeD

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,41 +1,17 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
 import { LogoIcon } from "@/svg/LogoIcon";
 import PermIdentityIcon from "@mui/icons-material/PermIdentity";
 
 const pages = ["Нүүр", "Хоолны цэс", "Хүргэлтийн бүс"];
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
     <AppBar
-      position="static"
       sx={{
         bgcolor: "white",
         height: 57,
@@ -43,29 +19,48 @@ function ResponsiveAppBar() {
         justifyItems: "center",
         alignItems: "center",
         boxShadow: "none",
+        width: "full",
       }}
     >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <LogoIcon />
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "black", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+      <Container
+        maxWidth="xl"
+        sx={{
+          paddingX: 4,
+          paddingY: 1,
+          display: "flex",
+          justifyItems: "justify-between",
+          alignItems: "center",
+        }}
+      >
+        <LogoIcon />
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: "flex",
+          }}
+        >
+          {pages.map((page) => (
+            <Button
+              key={page}
+              sx={{
+                // my: 2,
+                color: "black",
+                // display: "block",
+                // text: 14,
+                // fontWeight: 700,
+                // lineHeight: 16,
+              }}
+            >
+              {page}
+            </Button>
+          ))}
+        </Box>
+        <Box sx={{ display: "flex", gap: 2 }}>
           <PermIdentityIcon sx={{ color: "text.primary" }} />
-          <MenuItem onClick={handleCloseNavMenu}>
-            <Typography sx={{ textAlign: "center", color: "text.primary" }}>
-              Нэвтрэх
-            </Typography>
-          </MenuItem>
-        </Toolbar>
+          <Typography sx={{ textAlign: "center", color: "text.primary" }}>
+            Нэвтрэх
+          </Typography>
+        </Box>
       </Container>
     </AppBar>
   );

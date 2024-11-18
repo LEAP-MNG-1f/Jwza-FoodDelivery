239f50dec6ab13820ea27fbd1ba112620444cd15
Жавзандулам Санжаасүрэн <24LP4023@G401-CC0100.local>
2024-11-18 11:30:36 +0800

    homepage hero end

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -10,10 +10,10 @@ import { SagsIcon } from "@/svg/SagsIcon";
 
 const pages = ["Нүүр", "Хоолны цэс", "Хүргэлтийн бүс"];
 
-function ResponsiveAppBar() {
+function Header() {
   return (
     <AppBar className="!bg-white !h-[57px] !flex !items-center !justify-center !shadow-none !w-full">
-      <Container className="!w-[1258px] !flex !items-center !justify-between !px-6 !py-2">
+      <Container className="!w-[1200px] !flex !items-center !justify-between !p-0">
         <div className="!flex !gap-[24px]">
           <LogoIcon />
           <div className="!flex !gap-[8px] ">
@@ -28,21 +28,21 @@ function ResponsiveAppBar() {
           </div>
         </div>
         <div className="!flex !gap-2">
-          <Box className="!flex !gap-[8px] !justify-center !items-center !px-4 !py-2">
+          <Button className="!flex !gap-[8px] !justify-center !items-center !px-4 !py-2">
             <SagsIcon />
             <Typography className="!text-[14px] !leading-[20px] !font-[700] !text-black !text-align">
               Сагс
             </Typography>
-          </Box>
-          <Box className="!flex !gap-[8px] !justify-center !items-center !px-4 !py-2">
+          </Button>
+          <Button className="!flex !gap-[8px] !justify-center !items-center !px-4 !py-2">
             <PermIdentityIcon sx={{ color: "text.primary" }} />
             <Typography className="!text-[14px] !leading-[20px] !font-[700] !text-black !text-align">
               Нэвтрэх
             </Typography>
-          </Box>
+          </Button>
         </div>
       </Container>
     </AppBar>
   );
 }
-export default ResponsiveAppBar;
+export default Header;
---
+++ front-end/src/components/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import Header from "../Header";
+import { HeroHomePage } from "../HomePage/HeroHomePage";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Header />
+      <HeroHomePage />
+    </div>
+  );
+}

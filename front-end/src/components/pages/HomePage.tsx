d17e1ed5e9ebefea7cdde806e00f87c875edb733
Jwz-jwz <jnglobal444@gmail.com>
2024-11-23 01:05:35 +0800

    dsds

--- front-end/src/components/pages/HomePage.tsx
+++ front-end/src/components/pages/HomePage.tsx
@@ -1,9 +1,9 @@
 import { Footer } from "../ui/Footer";
 import Header from "../ui/Header";
 import { FoodHomePage, FoodHomePageProps } from "../HomePage/BodyHomePage";
+import { MockDataOrder } from "./OrderPage";
 import { HeroHomePage } from "../homepage/HeroHomePage";
 import InfoCard from "../homepage/InfoCard";
-import { MockDataOrder } from "./OrderPage";
 
 export default function HomePage({ foods }: FoodHomePageProps) {
   return (

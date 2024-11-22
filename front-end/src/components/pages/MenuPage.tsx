b13f640201f9a6eb8eb15b69de3db4656babc21f
Jwz-jwz <jnglobal444@gmail.com>
2024-11-23 01:22:45 +0800

    not-end

--- front-end/src/components/pages/MenuPage.tsx
+++ front-end/src/components/pages/MenuPage.tsx
@@ -1,8 +1,8 @@
 import { Footer } from "../ui/Footer";
 import Header from "../ui/Header";
-import { FoodHomePageProps } from "../homepage/BodyHomePage";
 import MenuBody from "../menupage/MenuBody";
 import { MockDataOrder } from "./OrderPage";
+import { FoodHomePageProps } from "../homepage/BodyHomePage";
 
 export default function MenuPage({ foods }: FoodHomePageProps) {
   return (
